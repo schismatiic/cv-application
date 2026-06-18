5265f7305cfeb4cf6b00ee14c14324d642954db7
schismatiic <leonardojofre2003@gmail.com>
2026-06-18 18:36:55 -0400

    Educational experience completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import General from "./components/General";
+import Educational from "./components/Educational";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <section id="header">
         <Header />
         <General />
+        <Educational />
       </section>
     </>
   );
---
+++ src/components/Educational.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "../styles/Educational.css";
+
+function Educational() {
+  const [boolMode, setBoolMode] = useState(true);
+  const [schoolName, setSchoolName] = useState("");
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const handleMode = () => setBoolMode((prev) => !prev);
+
+  return (
+    <div className="educational">
+      <h3>Educational experience</h3>
+      {boolMode ? (
+        <>
+          {" "}
+          <div className="educational-field">
+            <label htmlFor="educational-name">School name: </label>
+            <input
+              onChange={(e) => setSchoolName(e.target.value)}
+              id="educational-name"
+              type="text"
+              placeholder="University of Oxford"
+              value={schoolName !== "" ? schoolName : ""}
+              required
+            />
+          </div>
+          <div className="educational-field">
+            <label htmlFor="educational-title">Title of study: </label>
+            <input
+              onChange={(e) => setTitle(e.target.value)}
+              id="educational-title"
+              type="text"
+              placeholder="Computer Science"
+              value={title !== "" ? title : ""}
+              required
+            />
+          </div>
+          <div className="educational-field">
+            <label htmlFor="educational-date">Date of study: </label>
+            <input
+              onChange={(e) => setDate(e.target.value)}
+              id="educational-date"
+              type="text"
+              placeholder="2020-2025"
+              value={date !== "" ? date : ""}
+              required
+            />
+          </div>{" "}
+        </>
+      ) : (
+        <>
+          <h4>{schoolName}</h4>
+          <p>{title}</p>
+          <p>{date}</p>
+        </>
+      )}
+      <button onClick={handleMode}>{boolMode ? "Submit" : "Edit"}</button>
+    </div>
+  );
+}
+export default Educational;
