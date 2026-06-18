90993486112ec4482bc381733664658168d2d577
schismatiic <leonardojofre2003@gmail.com>
2026-06-18 16:34:29 -0400

    General information section created

---
+++ src/components/General.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "../styles/General.css";
+
+function General() {
+  const [boolMode, setBoolMode] = useState(true);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const handleMode = () => setBoolMode((prev) => !prev);
+
+  return (
+    <div className="general">
+      <h3>General information</h3>
+      {boolMode ? (
+        <>
+          {" "}
+          <div className="general-field">
+            <label htmlFor="general-name">Full name: </label>
+            <input
+              onChange={(e) => setFullName(e.target.value)}
+              id="general-name"
+              type="text"
+              placeholder="John Doe"
+              required
+            />
+          </div>
+          <div className="general-field">
+            <label htmlFor="general-email">Email: </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              id="general-email"
+              type="email"
+              placeholder="example@example.com"
+              required
+            />
+          </div>
+          <div className="general-field">
+            <label htmlFor="general-phone">Phone number: </label>
+            <input
+              onChange={(e) => setPhone(e.target.value)}
+              id="general-phone"
+              type="tel"
+              placeholder="+1 (555) 555-5555"
+              required
+            />
+          </div>{" "}
+        </>
+      ) : (
+        <>
+          <h4>{fullName}</h4>
+          <p>{email}</p>
+          <p>{phone}</p>
+        </>
+      )}
+      <button onClick={handleMode}>{boolMode ? "Submit" : "Edit"}</button>
+    </div>
+  );
+}
+export default General;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../styles/Header.css";
+function Header() {
+  return (
+    <header className="header">
+      <h1>CV Application</h1>
+      <h2>Build and customize your professional CV</h2>
+      <p>
+        Create a professional CV by filling out your personal information,
+        education, and work experience. Review your changes, edit any section at
+        any time, and keep your résumé up to date.
+      </p>
+    </header>
+  );
+}
+export default Header;
